517a57d852dc24202c137544c0a2cd430e2772e5
Sam Buchmiller <buc11003@byui.edu>
2013-10-11 13:14:20 -0600

    Added first prototype
    This project demos how to set up movement with the w,a,s,d keys.

---
+++ Movement Prototype/MyFirstProject/Assets/CubeBehaviourScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var walkspeed: float = 5.0;
+
+function Start() {
+
+
+
+}
+
+function Update() {
+
+    rigidbody.freezeRotation = true;
+
+    if (Input.GetKey("w")) transform.Translate(Vector3(0, 0, 1) * Time.deltaTime * walkspeed);
+    if (Input.GetKey("s")) transform.Translate(Vector3(0, 0, - 1) * Time.deltaTime * walkspeed);
+    if (Input.GetKey("a")) transform.Translate(Vector3(-1, 0, 0) * Time.deltaTime * walkspeed);
+    if (Input.GetKey("d")) transform.Translate(Vector3(1, 0, 0) * Time.deltaTime * walkspeed);
+
+}
