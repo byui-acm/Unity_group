6277ca4c4da5a8c4c09d3b9ae7297330477f0df6
stake5 <stakerbaker19@gmail.com>
2013-11-07 15:17:13 -0700

    Finished demo and created runnable for demo

---
+++ LightDarkUnityDemo/Assets/Playlist.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Playlist : MonoBehaviour
+{
+	public List<AudioClip> musicPlaylist = new List<AudioClip>();
+	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!audio.isPlaying)
+		{
+			playRandomSong();
+		}
+	}
+	
+	private void playRandomSong()
+	{
+		audio.clip = musicPlaylist[Random.Range(0, musicPlaylist.Count)];
+		audio.Play();
+	}
+}
