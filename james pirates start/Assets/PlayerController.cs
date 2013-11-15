b27981e980bd59d5a9a3b2cabaa9861b68ac008f
stake5 <stakerbaker19@gmail.com>
2013-11-14 19:45:54 -0700

    started the pirate game

---
+++ james pirates start/Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5;
+	public float rotationSpeed = 5;
+	public float jumpspeed = .25f;
+	public bool isJumping;
+	Vector3 jumpVec;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		isJumping = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Moves player left, right, up, down. No collision.
+    	float x = Input.GetAxis("Horizontal") * Time.smoothDeltaTime * speed;
+    	
+		if (isJumping == true)
+		{
+			transform.position += jumpVec * Time.deltaTime;
+			jumpVec += (Vector3.down * Time.deltaTime);
+		}
+		
+		else
+		{
+			
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				isJumping = true;
+				jumpVec = Vector3.up * (jumpspeed * Time.smoothDeltaTime);
+				//transform.Translate(Vector3.up * jumpspeed * Time.deltaTime, Space.World);
+				//rigidbody.AddForce(Vector3.up * jumpspeed);
+			}
+		}
+
+		transform.Translate(x, 0, 0, Space.World); //move Player
+		
+		//move main camera
+		//Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 12.5f, transform.position.z - 2.5f);
+		
+		//move light volume
+		//GameObject.FindGameObjectWithTag("Light volume").transform.position = transform.position;
+		
+		//Rotate Player to face where your mouse pointer is
+		//Plane playerPlane = new Plane(Vector3.up, transform.position);
+    	//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    	//var hitdist = 0.0f;
+ 
+    	//if (playerPlane.Raycast(ray, out hitdist))
+		//{
+ 	    //	var targetPoint = ray.GetPoint(hitdist);
+        //	var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+        //	transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.smoothDeltaTime * 2);
+			
+			//rotate light volume
+		//	GameObject.FindGameObjectWithTag("Light volume").transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.smoothDeltaTime * 2);
+    	//}
+	}
+	
+	void OnCollisionEnter (Collision hit)
+{
+    isJumping = false;
+    // check message upon collition for functionality working of code.
+    Debug.Log ("I am colliding with something");
+}
+}
